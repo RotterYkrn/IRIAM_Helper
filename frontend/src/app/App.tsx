ef6d71b422bbf057d1e00ca106ff2bf7d4f2b87c
RotterYkrn <sugaku.ohno@gmail.com>
2026-01-25 00:38:58 +0900

    supabase構築

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,5 +1,18 @@
-const App: React.FC = () => {
-    return <></>;
-};
+import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
 
+const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY,
+);
+
+function App() {
+    const [time, setTime] = useState("");
+
+    useEffect(() => {
+        supabase.rpc("now").then(({ data }) => setTime(data));
+    }, []);
+
+    return <div>Supabase time: {time}</div>;
+}
 export default App;
