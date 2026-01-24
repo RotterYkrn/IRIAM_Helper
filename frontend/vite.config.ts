260c950e7229a3288a5d433066bb87e481946411
RotterYkrn <sugaku.ohno@gmail.com>
2026-01-25 00:47:11 +0900

    vite設定修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,22 +5,9 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    // base: "./",
-    // root: "./src",
     build: {
         outDir: resolve(__dirname, "dist"),
         emptyOutDir: true,
     },
     plugins: [tsconfigPaths(), react()],
-
-    server: {
-        host: true,
-        proxy: {
-            "/api": {
-                target: "http://backend:3000",
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, "/api"),
-            },
-        },
-    },
 });
