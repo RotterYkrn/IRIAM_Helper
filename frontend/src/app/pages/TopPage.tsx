3f7e4db4231508f561eff44662c4c10b114acf27
RotterYkrn <sugaku.ohno@gmail.com>
2026-02-16 03:07:54 +0900

    トップページ作成

--- frontend/src/app/pages/TopPage.tsx
+++ frontend/src/app/pages/TopPage.tsx
@@ -1,5 +1,12 @@
+import ProjectTopLayout from "../components/projects/ProjectTopLayout";
+
 const TopPage = () => {
-    return <div>TopPage（仮）</div>;
+    return (
+        <div className="flex flex-col items-center justify-center space-y-4">
+            <h1 className="text-3xl font-bold">企画一覧</h1>
+            <ProjectTopLayout />
+        </div>
+    );
 };
 
 export default TopPage;
