d7647bc77bfb7a3e8f0ad4113c9332af7c74040b
RotterYkrn <sugaku.ohno@gmail.com>
2026-02-16 03:16:32 +0900

    新規作成ボタンをトップにも追加

--- frontend/src/app/pages/TopPage.tsx
+++ frontend/src/app/pages/TopPage.tsx
@@ -1,9 +1,13 @@
+import CreateProjectButton from "../components/projects/CreateProjectButton";
 import ProjectTopLayout from "../components/projects/ProjectTopLayout";
 
 const TopPage = () => {
     return (
         <div className="flex flex-col items-center justify-center space-y-4">
             <h1 className="text-3xl font-bold">企画一覧</h1>
+            <div className="flex w-150 justify-end">
+                <CreateProjectButton toggleSidebar={() => {}} />
+            </div>
             <ProjectTopLayout />
         </div>
     );
