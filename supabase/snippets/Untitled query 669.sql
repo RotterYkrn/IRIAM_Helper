7a232b770540fd7a9a7c6918beb12a8dfc498310
RotterYkrn <sugaku.ohno@gmail.com>
2026-02-16 06:56:33 +0900

    テーブルだけ作った

--- supabase/snippets/Untitled query 669.sql
+++ supabase/snippets/Untitled query 669.sql
@@ -1,9 +1,6 @@
-DROP TYPE IF EXISTS update_endurance_action_input cascade;
-
--- 定義したカスタム型を一覧表示する
-SELECT n.nspname as schema, t.typname as type_name
-FROM pg_type t
-LEFT JOIN pg_namespace n ON n.oid = t.typnamespace
-WHERE n.nspname = 'public' 
-  AND t.typtype = 'c' -- 'c' は composite type (複合型)
-order by type_name;
+ALTER TABLE projects
+    -- 一度削除
+    DROP CONSTRAINT projects_type_check,
+    -- 新しいリストで再作成
+    ADD CONSTRAINT projects_type_check 
+        CHECK (type IN ('endurance', 'multi_endurance', 'gacha', 'panel_open'));
