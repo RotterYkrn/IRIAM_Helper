414baec0be90c44b5206f46022eef0bb80c79558
RotterYkrn <sugaku.ohno@gmail.com>
2026-02-20 21:49:39 +0900

    バージョン情報追加

--- frontend/vite-env.d.ts
+++ frontend/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+    readonly PACKAGE_VERSION: string;
     readonly VITE_SUPABASE_URL: string;
     readonly VITE_SUPABASE_PUBLISHABLE_KEY: string;
 }
