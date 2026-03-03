4c9eeb336e9b484ce32c160ab72ecbaec758a47a
RotterYkrn <sugaku.ohno@gmail.com>
2026-03-03 19:43:47 +0900

    atomの説明用コメントを追加

--- frontend/src/atoms/endurances-new/isEditEnduranceValidAtom.ts
+++ frontend/src/atoms/endurances-new/isEditEnduranceValidAtom.ts
@@ -8,6 +8,9 @@ import {
 } from "./EditActionAtom";
 import { editTargetCountErrorAtomNew } from "./EditTargetCountAtom";
 
+/**
+ * 耐久企画に関する編集内容が有効なものかどうかを格納するAtom
+ */
 export const isEnduranceValidAtomNew = atom((get) => {
     return (
         !get(editTitleErrorAtom) &&
