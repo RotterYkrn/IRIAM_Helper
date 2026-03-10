31a7246df322593fed2a21069cfcb997429dbfa3
RotterYkrn <sugaku.ohno@gmail.com>
2026-03-11 00:46:47 +0900

    変換場所を調整した

--- frontend/src/app/components/projects/ProjectSideBarLayout.tsx
+++ frontend/src/app/components/projects/ProjectSideBarLayout.tsx
@@ -1,6 +1,6 @@
 import ProjectSideBarGroup from "./ProjectSideBarGroup";
 
-import { useFetchProjectForSideBar } from "@/hooks/projects/useFetchProjectForSideBar";
+import { useFetchProjectForList } from "@/hooks/projects/useFetchProjectForList";
 
 type Props = {
     /** 各企画が選択されたときに、サイドバーを閉じるために使います。 */
@@ -14,7 +14,7 @@ type Props = {
  * 開催予定、開催中、過去の企画を分類して表示します。
  */
 const ProjectSideBarLayout = ({ toggleSidebar }: Props) => {
-    const { data, isLoading } = useFetchProjectForSideBar();
+    const { data, isLoading } = useFetchProjectForList();
 
     if (isLoading) {
         return <div>読み込み中...</div>;
