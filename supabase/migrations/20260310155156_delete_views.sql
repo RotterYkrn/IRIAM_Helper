880885fd6f00e745893e1dab40f83f9856ced383
RotterYkrn <sugaku.ohno@gmail.com>
2026-03-11 01:24:05 +0900

    viewを削除

---
+++ supabase/migrations/20260310155156_delete_views.sql
@@ -0,0 +1,11 @@
+drop type "public"."endurance_action_stat";
+
+drop type "public"."endurance_action_stat_new";
+
+drop view if exists "public"."endurance_action_stats_view";
+
+drop view if exists "public"."endurance_action_stats_view_new";
+
+drop view if exists "public"."endurance_project_view";
+
+drop view if exists "public"."endurance_project_view_new";
