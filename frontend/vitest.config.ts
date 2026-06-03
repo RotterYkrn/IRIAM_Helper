4558e361015c94fac67a96548ddbd177625fb75d
RotterYkrn <sugaku.ohno@gmail.com>
2026-06-03 18:04:06 +0900

    ライブラリ設定

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,15 +1,16 @@
+import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+    plugins: [react(), tsconfigPaths()],
     test: {
         environment: "jsdom",
         globals: true,
         include: ["tests/**/*.test.ts"],
+        setupFiles: "./vitest.setup.ts",
         // coverage: {            // カバレッジ設定 (オプション)
         //   reporter: ['text', 'json', 'html'],
         // },
     },
-
-    plugins: [tsconfigPaths()],
 });
